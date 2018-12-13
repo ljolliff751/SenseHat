3b874c822b9efa927f40a983593a0df43b9f9caf
ljolliff751 <45638718+ljolliff751@users.noreply.github.com>
2018-12-13 13:24:57 -0600

    Update magic8ball.py

--- magic8ball.py
+++ magic8ball.py
@@ -17,4 +17,16 @@ replies = ['Signs point to yes',
            'Outlook not so good'
            ]
 
+while True:
+    x, y, z = sense.get_accelerometer_raw().values()
+
+    x = abs(x)
+    y = abs(y)
+    z = abs(z)
+
+    if x > 2 or y > 2 or z > 2 :
+        sense.show_message(choice(replies))
+    else:
+        sense.clear()
+
 sense.show_message(choice(replies))
