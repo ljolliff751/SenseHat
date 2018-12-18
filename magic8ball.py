d50637611581bbeaee31200039a9cd3ae18cfa07
ljolliff751 <45638718+ljolliff751@users.noreply.github.com>
2018-12-17 21:09:07 -0600

    Update magic8ball.py

--- magic8ball.py
+++ magic8ball.py
@@ -4,7 +4,25 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-sense.show_message("Ask a Question")
+I = (255, 255, 255) # White
+X = (0, 0, 0) # Black
+O = (128, 0, 128) # Purple
+
+ball_pixels = [
+    O, O, O, O, O, O, O, O,
+    O, O, X, X, X, X, O, O,
+    O, X, X, I, I, X, X, O,
+    O, X, I, I, I, I, X, O,
+    O, X, I, I, I, I, X, O,
+    O, X, X, I, I, X, X, O,
+    O, O, X, X, X, X, O, O,
+    O, O, O, O, O, O, O, O
+]
+
+sense.set_pixels(ball_pixels)
+sleep(5)
+
+sense.show_message("Ask a Question", text_colour=(255, 255, 255), back_colour=(128, 0, 128))
 sleep(1)
 
 replies = ['Signs point to yes',
@@ -28,8 +46,8 @@ while True:
     z = abs(z)
 
     if x > 2 or y > 2 or z > 2 :
-        sense.show_message(choice(replies))
+        sense.show_message(choice(replies), text_colour=(255, 255, 255), back_colour=(128, 0, 128))
     else:
         sense.clear()
 
-sense.show_message(choice(replies))
+sense.show_message(choice(replies), text_colour=(255, 255, 255), back_colour=(128, 0, 128))
